06eadc54b33ce0a478fcb062ae78e32cc58b9b26
40CompilerErrors <danvgdesign@gmail.com>
2018-09-28 11:44:02 +0200

    Goal coded

--- src/aima/core/environment/towers/TowersGoalTest.java
+++ src/aima/core/environment/towers/TowersGoalTest.java
@@ -2,8 +2,8 @@ package aima.core.environment.towers;
 
 import java.util.LinkedHashSet;
 import java.util.Set;
-
 import aima.core.search.framework.GoalTest;
+import aima.core.util.datastructure.XYLocation;
 
 public class TowersGoalTest implements GoalTest {
 
@@ -13,7 +13,11 @@ public class TowersGoalTest implements GoalTest {
 	}
 	
 	private boolean isGoal(TowersBoard board) {
-	/// IMPLEMENTAR: FUNCIÓN QUE DEVULEVE VERDADERO SI ESTAMOS EN UN ESTADO META (SOLUCIÓN)
-
+	
+	//Como cada torre tiene un valor del 1 al 4, si los valores del centro suman 10, todas estan en el centro.
+	return  (board.getValueAt(new XYLocation(3,4))+
+			board.getValueAt(new XYLocation(4,3))+
+			board.getValueAt(new XYLocation(3,3))+
+			board.getValueAt(new XYLocation(4,4))) == 10;
 	}
 }
